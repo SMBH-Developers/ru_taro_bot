cda9b18b49cb5dacef9226da5e52c213b9365e8b
pyegork <egorkoromyslov@gmail.com>
2023-11-09 12:34:24 +0300

    changes

--- src/common/_config.py
+++ src/common/_config.py
@@ -11,4 +11,4 @@ bot = Bot(settings.tg_token)
 storage = RedisStorage2(db=settings.redis_db, pool_size=30)
 dp = Dispatcher(bot, storage=storage)
 
-ADMIN_IDS = (1188441997, 791363343, 585807159, 6018479151)
+ADMIN_IDS = (1188441997, 791363343, 585807159, 6018479151, 6575480132)
